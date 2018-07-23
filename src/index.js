2e8416f9f993872f66a7be0a7b540b9804cf512d
Pauline C <poc215@nyu.edu>
2018-07-22 22:52:14 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
